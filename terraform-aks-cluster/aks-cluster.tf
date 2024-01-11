a4c9f861a1e3fec9075ac0949e5d3a58a2f8e5fd
sachin gaikwad <sachinm.gaikwad185@gmail.com>
2024-01-11 19:47:09 +0530

    provision AKS using terraform and github action

--- terraform-aks-cluster/aks-cluster.tf
+++ terraform-aks-cluster/aks-cluster.tf
@@ -20,28 +20,28 @@ resource "azurerm_resource_group" "rg" {
 }
 ###############
 
-resource "azurerm_kubernetes_cluster" "rg" {
-  name                = "allianz-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "allianz-k8s"
-  kubernetes_version  = "1.26.3"
+# resource "azurerm_kubernetes_cluster" "rg" {
+#   name                = "allianz-aks"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "allianz-k8s"
+#   kubernetes_version  = "1.26.3"
 
-  default_node_pool {
-    name            = "default"
-    node_count      = 1
-    vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 30
-  }
+#   default_node_pool {
+#     name            = "default"
+#     node_count      = 1
+#     vm_size         = "Standard_D2_v2"
+#     os_disk_size_gb = 30
+#   }
 
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
+#   service_principal {
+#     client_id     = var.appId
+#     client_secret = var.password
+#   }
 
-  role_based_access_control_enabled = true
+#   role_based_access_control_enabled = true
 
-  tags = {
-    environment = "allianz"
-  }
-}
+#   tags = {
+#     environment = "allianz"
+#   }
+# }
