cd1c9b77392f0241280546d0d4f7ef0879612c4f
sachin gaikwad <sachinm.gaikwad185@gmail.com>
2024-01-11 11:40:43 +0530

    Updated iris module

--- iris/app.py
+++ iris/app.py
@@ -1,25 +1,32 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
 # Load your trained machine learning model
-model = joblib.load('IRIS-model.pkl')
+file_path = 'iris_model_01.pkl'
+model = None
+# load the model
+with open(file_path, 'rb') as file:
+    model = pickle.load(file)
 
-@app.route('/predict', methods=['GET', 'POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get data from the request
         data = request.get_json(force=True)
         
-        # Assuming the data contains features 'sepal_length', 'sepal_width', 'petal_length', 'petal_width'
-        features = [data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]
+        # Assuming the data contains features 'SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm'
+        features = [data['SepalLengthCm'], data['SepalWidthCm'], data['PetalLengthCm'], data['PetalWidthCm']]
         # features = [['sepal_length'],'[sepal_width]','[petal_length]', '[petal_width]']
-        
-        # Make a prediction
-        prediction = model.predict(np.array(features).reshape(1, -1))
 
+        if model is None:
+            print("Model has not been loaded properly!")
+        else:
+            prediction = model.predict(np.array(features).reshape(1, -1))
         # Return the prediction as JSON
         return jsonify({'prediction': prediction.tolist()})
 
@@ -28,3 +35,4 @@ def predict():
 
 if __name__ == '__main__':
     app.run(port=5000)
+
